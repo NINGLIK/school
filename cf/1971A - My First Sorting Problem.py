f7176e1f73823e4219d2bb14570b9519566a41ee
NINGLIK <2573115865@qq.com>
2024-05-27 00:20:00 +0800

    /

--- cf/1971A - My First Sorting Problem.py
+++ cf/1971A - My First Sorting Problem.py
@@ -1,13 +1,9 @@
 def function(number1, number2):
     if number1 > number2:
-        # 交换 number1 和 number2 的值
         number1, number2 = number2, number1
     return number1, number2
 
-# 提示用户输入总的输入次数
 data = int(input())
-
-# 循环输入数据并处理
 for i in range(data):
     user_input = input()
     num1, num2 = map(int, user_input.split())
